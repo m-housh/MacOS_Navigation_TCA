0b99ec64228a0a6562eb51665e6ed8beb7358fbe
Michael Housh <mhoush@houshhomeenergy.com>
2024-02-25 14:54:53 -0500

    Initial commit

--- MacOS_Navigation_TCA/ContentView.swift
+++ MacOS_Navigation_TCA/ContentView.swift
@@ -1,24 +1,85 @@
-//
-//  ContentView.swift
-//  MacOS_Navigation_TCA
-//
-//  Created by Michael on 2/25/24.
-//
-
+import ComposableArchitecture
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+@Reducer
+struct SimpleFeature {
+  @ObservableState
+  struct State: Equatable {
+    var text: String
+  }
+  
+  enum Action { }
+}
+
+struct SimpleFeatureView: View {
+  let store: StoreOf<SimpleFeature>
+  
+  var body: some View {
+    VStack {
+      Text(store.text)
+        .font(.title)
     }
+  }
+}
+
+@Reducer
+struct AppFeature: Reducer {
+  @ObservableState
+  struct State: Equatable {
+    @Presents var destination: SimpleFeature.State?
+  }
+  
+  enum Action: BindableAction {
+    case binding(BindingAction<State>)
+    case destination(PresentationAction<SimpleFeature.Action>)
+    case buttonTapped
+  }
+  
+  var body: some Reducer<State, Action> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+      case .binding:
+        return .none
+      case .destination:
+        return .none
+      case .buttonTapped:
+        state.destination = .init(text: "I'm a simple feature")
+        return .none
+      }
+    }
+  }
+}
+
+struct ContentView: View {
+  @Bindable var store: StoreOf<AppFeature>
+  
+  var body: some View {
+    NavigationStack {
+      VStack {
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundStyle(.tint)
+        Text("Hello, world!")
+        
+        Button("Press Me") {
+          store.send(.buttonTapped)
+        }
+      }
+      .padding()
+      .navigationDestination(
+        item: $store.scope(state: \.destination, action: \.destination)
+      ) { store in
+        SimpleFeatureView(store: store)
+      }
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView(
+    store: Store(initialState: AppFeature.State()) {
+      AppFeature()
+    }
+  )
 }
--- MacOS_Navigation_TCA/MacOS_Navigation_TCAApp.swift
+++ MacOS_Navigation_TCA/MacOS_Navigation_TCAApp.swift
@@ -1,17 +1,15 @@
-//
-//  MacOS_Navigation_TCAApp.swift
-//  MacOS_Navigation_TCA
-//
-//  Created by Michael on 2/25/24.
-//
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct MacOS_Navigation_TCAApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  var body: some Scene {
+    WindowGroup {
+      ContentView(
+        store: Store(initialState: AppFeature.State()) {
+          AppFeature()
         }
+      )
     }
+  }
 }
