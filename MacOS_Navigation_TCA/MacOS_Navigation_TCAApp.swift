b88bbfa3815e4ecd5035d376133a0e0b8802530a
Michael Housh <mhoush@houshhomeenergy.com>
2024-02-25 14:34:56 -0500

    Initial Commit

---
+++ MacOS_Navigation_TCA/MacOS_Navigation_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_Navigation_TCAApp.swift
+//  MacOS_Navigation_TCA
+//
+//  Created by Michael on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_Navigation_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
